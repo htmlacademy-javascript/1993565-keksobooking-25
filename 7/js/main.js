a5d3be6bcc187bb3ccbd3c4d9af90f7896688f58
Keks <keks@htmlacademy.ru>
2022-03-29 01:28:51 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,16 @@
+import {
+  // eslint-disable-next-line no-unused-vars
+  getRandom,
+  // eslint-disable-next-line no-unused-vars
+  getDot,
+  // eslint-disable-next-line no-unused-vars
+  getRandLengthArray,
+  // eslint-disable-next-line no-unused-vars
+  getRandomArrayElement,
+} from './util.js';
+import './template.js';
+import { getSimilarApparts } from './data.js';
+
+getSimilarApparts();
+//console.log(similarAppars);
+//renderBlaBla(similarApparts[0]);
